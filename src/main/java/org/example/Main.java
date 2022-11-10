9ac2f0fe39e368890fa5a8658fe2876396c1acc9
coppmc <117922211+coppmc@users.noreply.github.com>
2022-11-10 14:52:50 +0000

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,8 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println("Hello again");
+        
+         System.out.println("Hello again again");
     }
 
 }
