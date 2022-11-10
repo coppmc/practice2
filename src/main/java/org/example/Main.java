568c9fd8e559cbdef77ef787f70449ed62c363f1
JWes89 <117924267+JWes89@users.noreply.github.com>
2022-11-10 15:22:39 +0000

    Update Main.java
    woohoo we did it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,8 @@ public class Main {
          System.out.println("Hello again again");
 
         System.out.println("Good Bye");
+        
+        System.out.println("Hiiii Molly *waves* ");
     }
 
 }
